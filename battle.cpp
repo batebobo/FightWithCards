adb8a8bbc705c5e948bfec0777345971564e3af2
batebobo <bvushkov@gmail.com>
2015-05-17 17:49:09 +0300

    battle.cpp changes

--- battle.cpp
+++ battle.cpp
@@ -20,7 +20,7 @@ void Battle::beginBattle()
 	srand(time(NULL));
 	firstPlayer = rand()%2;
 
-	while(P1.getHero().getHP() > 0 && P2.getHero().getHP() > 0)
+	while(P1.getHero().getHP() > 0 && P2.getHero().getHP() > 0) 
 	{
 		if(firstPlayer == 0)
 		{
@@ -42,6 +42,8 @@ void Battle::beginBattle()
 
 void Battle::play(Player& currentTurnPlayer, Player& inactivePlayer)
 {
+	//If Player.hand is empty, automatically end turn;
+	//Optional - bool isMonster to check the card type
 	bool endTurn = false;
 	while(!endTurn)
 	{
@@ -50,29 +52,38 @@ void Battle::play(Player& currentTurnPlayer, Player& inactivePlayer)
 		{
 			int k;
 			cout<<"Press 9 to end your turn."<<endl;
-			cout<<"Press 1 to set monster on the battlefield."<<endl;
-			cout<<"Press 2 to use spell card."<<endl;
+			cout<<"Press 0 to set monster on the battlefield."<<endl;
+			cout<<"Press 1 to use spell card on hero." << endl;
+			cout<<"Press 2 to use spell card on monster."<<endl;
 			cout<<"Press 3 to attack enemy monster."<<endl;
 			cout<<"Press 4 to attack enemy hero."<<endl;
 			cout<<"Press 5 to use hero skill."<<endl;
 			cin >> k;
 			switch(k) 
 			{
-				case 1 : 
+				case 0 : // Monster use
 					currentTurnPlayer.hand.printHand();
 					cout << "Enter the name of the monster you want to play!" << endl;
 					int index; cin >> index;
 					currentTurnPlayer.useCard(index); //if monster, and if valid index // Error - private access
+					summonedMonster = true;
+
+					//Player.useCard(n) - puts n-th card from Player.hand in the field array;
 				break;
-				case 2 :
+				case 1: // SpellCard on hero
+	
+				break;
+				case 2 : //SpellCard use
 					currentTurnPlayer.hand.printHand(); //Error - private access
 					cout << "Enter the index of the spell card you want to play!" << endl;
 					int index; cin >> index;
 					currentTurnPlayer.useCard(index); //if spell and if valid index // Error - private access
+					//Player.useCard(n) - plays n-th card from Player.hand *if it's a spell*;
 				break;
 				case 3 : 
-					//first - with which monster to attack - attacking player field
-					//second - which monster to be attacked by first - defending player field
+					//If Player.field is empty, skip this step
+					//first - with which monster to perform the attack - from attacking player field
+					//second - which monster to be attacked by first - from defending player field
 
 					cout << "Enter the index with which you want to attack the enemy!" << endl;
 					int attackingIndex; cin >> attackingIndex;
@@ -80,13 +91,16 @@ void Battle::play(Player& currentTurnPlayer, Player& inactivePlayer)
 
 					cout << "Enter the index of the monster you wish to attack!" << endl;
 					int defendingIndex; cin >> defendingIndex;
+					inactivePlayer.field[defendingIndex].printCard(); //Error - private access
 					// attack inactivePlayer.field[defendingIndex]
 					currentTurnPlayer.field[attackingIndex].attack(inactivePlayer.field[defendingIndex]); //Error - Card has no function "Attack", Private access
-
-
 				break;
-				case 4 : ; break;
-				case 5 : ; break;
+				case 4 : 
+					currentTurnPlayer.hero.attack(inactivePlayer.hero); //Error - private access
+					; break;
+				case 5 : 
+					currentTurnPlayer.hero.usePower(inactivePlayer.hero); //Error - private access
+					; break;
 				case 9 : endTurn = true; break;
 				default : cout<<"Incorrect command input!"<<endl; 
 
@@ -94,12 +108,14 @@ void Battle::play(Player& currentTurnPlayer, Player& inactivePlayer)
 		}
 		else
 		{
+			//Same as above - just without the option to play a monster
 			int k;
 			cout<<"Press 9 to end your turn."<<endl;
 			cout<<"Press 1 to use spell card."<<endl;
 			cout<<"Press 2 to attack enemy monster."<<endl;
 			cout<<"Press 3 to attack enemy hero."<<endl;
 			cout<<"Press 4 to use hero skill."<<endl;
+			cin >> k;
 			switch(k) 
 			{
 				case 1 : ;
