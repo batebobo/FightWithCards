49772d52dc98f7b1fc93a898c62afe0de2c4b8b8
sifon4o <j.serbezki@gmail.com>
2015-06-01 11:51:02 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -19,8 +19,28 @@ using namespace std;
 
 int main()
 {
-    Monster murloc("murloc",1,1,3,4);
-    SpellCard frostbolt(SpellCard::damageHero,"frostbolt",2,2,3);
+    Monster murloc("Murloc Raider",1,1,2,1);
+    Monster raptor("Bloodfen Raptor",2,2,3,2);
+    Monster tank("Spider Tank", 3, 3, 3, 4);
+    Monster yeti("Chillwind Yeti", 4, 4, 4, 5);
+    Monster dog("Salty Dog", 5, 5, 7, 4);
+    Monster ogre("Boulderfist Ogre", 6, 6, 6, 7);
+    Monster golem("War Golem", 7, 7, 7, 7);
+    Monster croco("River Crocolisk", 8, 2, 2, 3);
+    Monster hound("Core Hound", 9, 7, 9, 5);
+    Monster strider("Lost Tallstrider", 10, 4, 5, 4);
+    Monster rager("Magma Rager", 11, 3, 5, 1);
+    Monster snapjaw("Oasis Snapjaw", 12, 4, 2, 7);
+    Monster stomper("Puddlestomper", 13, 2, 3, 2);
+    Monster wisp("Wisp", 14, 0, 1, 1);
+    
+    SpellCard darkbomb(SpellCard::damageHero,"Dark Bomb", 15, 2, 3);
+    SpellCard arcaneshot(SpellCard::damageHero, "Arcane Shot", 16, 1, 2);
+    SpellCard arcaneshot(SpellCard::damageHero, "Fireball", 17, 4, 6);
+    SpellCard pyroblast(SpellCard::damageHero, "Pyroblast", 18, 10, 10);
+    SpellCard blessing(SpellCard::buffCreature, "Blessing of Kings", 19, 4, 4);
+    SpellCard healing(SpellCard::healYourself, "Healing Touch", 20, 3, 8);
+    
     Card* cards[30];
     for(int i = 0 ; i<30;i++){
         if(i%2==0){
