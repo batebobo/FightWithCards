e7f1394640a8fd716f61940dc8ed6beb47e7c4c4
sifon4o <j.serbezki@gmail.com>
2015-06-07 16:35:49 +0300

    Update warrior.h

--- warrior.h
+++ warrior.h
@@ -9,9 +9,7 @@ public:
 	Warrior(char* = "Warrior", int = 30, int = 2, bool=false);
 
 	void print() const;
-	void usePower(Hero*, Monster*, int);
-
-	//bool hasDifferentTargets() const { return=false; }
+	void usePower(Player*, Player*);
 };
 
 #endif
